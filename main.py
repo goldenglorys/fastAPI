81b8e224d2cbb3d2576132b67505c9c549b6fcd1
Olusola Glory <olusolagloryolamide@gmail.com>
2020-10-14 16:33:30 +0100

    query parameter code added

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
+
 
 class Item(BaseModel):
     name: str
@@ -54,3 +60,8 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
